2d9fa617014f47ee80d3de5d62e0357caeac2a81
jay <emma.jordan@bsu.edu>
2024-10-15 18:29:06 -0400

    correct usage of router

---
+++ week7assignment/src/app/appetizers/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useRouter } from "next/navigation";
+
+export default function() {
+  const router = useRouter();
+  
+  return(
+    <><text>
+      <ul>
+  <li><button onClick={() => router.push('/home')}>
+      Home
+    </button></li>
+    <li><button onClick={() => router.push('/entrees')}>
+        Entrees
+      </button></li>
+
+      <li>Wings, Potato Skins, Crab Cakes, Bruschetta</li>
+    </ul></text></>
+)
+}
---
+++ week7assignment/src/app/homepage/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useRouter } from "next/navigation";
+
+export default function home() {
+  const router = useRouter();
+  
+  return(
+  <><ul>
+    <li><button onClick={() => router.push('/')}>
+      Home
+    </button></li>
+    <li><button onClick={() => router.push('/entrees')}>
+        Entrees
+    </button></li>
+    <li><button onClick={() => router.push('/appetizers')}>
+        Appetizers
+    </button></li>
+    </ul></>
+)
+}
