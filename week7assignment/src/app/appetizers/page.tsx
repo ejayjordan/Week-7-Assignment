00f66a336ab65f2a6def76569e786fe44b9af1aa
jay <emma.jordan@bsu.edu>
2024-10-15 18:54:07 -0400

    link to Home using Layout.tsx

--- week7assignment/src/app/appetizers/page.tsx
+++ week7assignment/src/app/appetizers/page.tsx
@@ -1,19 +1,10 @@
 'use client'
-import { useRouter } from "next/navigation";
 
 export default function() {
-  const router = useRouter();
   
   return(
     <><text>
       <ul>
-  <li><button onClick={() => router.push('/')}>
-      Home
-    </button></li>
-    <li><button onClick={() => router.push('/entrees')}>
-        Entrees
-      </button></li>
-
       <li>Wings, Potato Skins, Crab Cakes, Bruschetta</li>
     </ul></text></>
 )
